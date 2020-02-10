a1a7dd9a8a8e0602369a082a3b8c2deda1c813df
Ruben <rvazquezcaceres@gmail.com>
2020-02-10 13:57:13 +0100

    Pods

---
+++ iOSiFoodie/HomeController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeController.swift
+//  iOSiFoodie
+//
+//  Created by alumnos on 05/02/2020.
+//  Copyright © 2020 VictorGarcia. All rights reserved.
+//
+
+import UIKit
+
+class HomeController : UIViewController {
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "fondoIFOODIE"))
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
